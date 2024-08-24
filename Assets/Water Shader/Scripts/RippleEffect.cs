4730f1421e890e2df820aafc83c6713b8209ab15
edanurarslan <116951700+edanurarslan@users.noreply.github.com>
2024-08-24 11:55:55 +0300

    third commit

---
+++ Assets/Water Shader/Scripts/RippleEffect.cs
@@ -0,0 +1,124 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class RippleEffect : MonoBehaviour
+{
+    public int TextureSize = 512;
+    public RenderTexture ObjectsRT;
+    private RenderTexture CurrRT, PrevRT, TempRT;
+    public Shader RippleShader, AddShader;
+    private Material RippleMat, AddMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Creating render textures and materials
+        CurrRT = new RenderTexture(TextureSize, TextureSize, 0, RenderTextureFormat.RFloat);
+        PrevRT = new RenderTexture(TextureSize, TextureSize, 0, RenderTextureFormat.RFloat);
+        TempRT = new RenderTexture(TextureSize, TextureSize, 0, RenderTextureFormat.RFloat);
+        RippleMat = new Material(RippleShader);
+        AddMat = new Material(AddShader);
+
+        //Change the texture in the material of this object to the render texture calculated by the ripple shader.
+        GetComponent<Renderer>().material.SetTexture("_RippleTex", CurrRT);
+
+        StartCoroutine(ripples());
+    }
+
+    // Update is called once per frame
+    IEnumerator ripples()
+    {
+        //Copy the result of blending the render textures to TempRT.
+        AddMat.SetTexture("_ObjectsRT", ObjectsRT);
+        AddMat.SetTexture("_CurrentRT", CurrRT);
+        Graphics.Blit(null, TempRT, AddMat);
+
+        RenderTexture rt0 = TempRT;
+        TempRT = CurrRT;
+        CurrRT = rt0;
+
+        //Calculate the ripple animation using ripple shader.
+        RippleMat.SetTexture("_PrevRT", PrevRT);
+        RippleMat.SetTexture("_CurrentRT", CurrRT);
+        Graphics.Blit(null, TempRT, RippleMat);
+        Graphics.Blit(TempRT, PrevRT);
+
+        //Swap PrevRT and CurrentRT to calculate the result for the next frame.
+        RenderTexture rt = PrevRT;
+        PrevRT = CurrRT;
+        CurrRT = rt;
+
+        //Wait for one frame and then execute again.
+        yield return null;
+        StartCoroutine(ripples());
+    }
+}
+*/
+
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class RippleEffect : MonoBehaviour
+{
+    public int TextureSize = 512;
+    public RenderTexture ObjectsRT;
+    private RenderTexture CurrRT, PrevRT, TempRT;
+    public Shader RippleShader, AddShader;
+    private Material RippleMat, AddMat;
+    public float rippleFrequency = 0.2f; // Ripple oluşum sıklığı
+    public float rippleStrength = 0.2f;  // Ripple gücü
+
+    void Start()
+    {
+        CurrRT = new RenderTexture(TextureSize, TextureSize, 0, RenderTextureFormat.RFloat);
+        PrevRT = new RenderTexture(TextureSize, TextureSize, 0, RenderTextureFormat.RFloat);
+        TempRT = new RenderTexture(TextureSize, TextureSize, 0, RenderTextureFormat.RFloat);
+        RippleMat = new Material(RippleShader);
+        AddMat = new Material(AddShader);
+
+        GetComponent<Renderer>().material.SetTexture("_RippleTex", CurrRT);
+
+        StartCoroutine(Ripples());
+        InvokeRepeating("AddRandomRipple", 0, rippleFrequency); // Ripple oluşturmayı başlat
+    }
+
+    IEnumerator Ripples()
+    {
+        while (true)
+        {
+            AddMat.SetTexture("_ObjectsRT", ObjectsRT);
+            AddMat.SetTexture("_CurrentRT", CurrRT);
+            Graphics.Blit(null, TempRT, AddMat);
+
+            RenderTexture rt0 = TempRT;
+            TempRT = CurrRT;
+            CurrRT = rt0;
+
+            RippleMat.SetTexture("_PrevRT", PrevRT);
+            RippleMat.SetTexture("_CurrentRT", CurrRT);
+            Graphics.Blit(null, TempRT, RippleMat);
+            Graphics.Blit(TempRT, PrevRT);
+
+            RenderTexture rt = PrevRT;
+            PrevRT = CurrRT;
+            CurrRT = rt;
+
+            yield return null;
+        }
+    }
+
+    void AddRandomRipple()
+    {
+        float x = Random.Range(0.0f, 1.0f);
+        float y = Random.Range(0.0f, 1.0f);
+        Vector4 rippleCenter = new Vector4(x, y, rippleStrength, 0.0f);
+        AddMat.SetVector("_RippleCenter", rippleCenter);
+        Graphics.Blit(null, ObjectsRT, AddMat);
+    }
+}
+
+
+
